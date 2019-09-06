44ff19511fd68306e26e35b3731212bcc29c6e45
Bob Costas <finkzane@gmail.com>
2019-09-06 15:29:21 -0700

    creation of files

---
+++ wolff_api_plugins/client/server_connection.py
@@ -0,0 +1,13 @@
+from message import Message
+
+class TCPServerConnection:
+    """
+        A connection to a server that takes place 
+        over the TCP/IP stack using TCP. 
+    """
+
+    def __init__( self ):
+        pass
+
+    def send( self, message ):
+        pass
