970c0259f8fb6f755450012863c1b776ae0edd6d
Bob Costas <finkzane@gmail.com>
2019-09-06 18:26:55 -0700

    added IP/port to TCPServerConnection

--- wolff_api_plugins/client/server_connection.py
+++ wolff_api_plugins/client/server_connection.py
@@ -6,8 +6,12 @@ class TCPServerConnection:
         over the TCP/IP stack using TCP. 
     """
 
-    def __init__( self ):
-        pass
+    def __init__( self, ip = "127.0.0.1",
+                  port = 0
+
+                ):
+        self._ip = ip
+        self._port = port
 
     def send( self, message ):
         pass
