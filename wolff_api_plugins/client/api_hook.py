e15825186cfeb683488eb16857e75bb534b9b6f8
Bob Costas <finkzane@gmail.com>
2019-09-09 18:19:04 -0700

    added methods necessary for an APIMethod

--- wolff_api_plugins/client/api_hook.py
+++ wolff_api_plugins/client/api_hook.py
@@ -15,26 +15,44 @@ class APIHook:
         pass
 
 class APIMethod:
-    def __init__( self ):
-        pass
+    """
+    Represents the data needed to 
+    call an API method. Has a uri for 
+    locating the resource necessary and 
+    a set of arguments that are included 
+    in the call of an API Method.
+    """
+    def __init__( self, uri = "", args = None, http_method = "" ):
+        self.uri = uri
+        self.args = args if args else None
+        self.http_method = http_method
 
     def get_http_method( self ):
-        pass
+        """
+        Get the HTTP method this 
+        APIMethod uses (GET, POST, etc.)
+        """
+        return self.http_method
 
     def get_uri( self ):
-        pass
+        return self.uri
 
     def args_as_dict( self ):
-        pass
+        return self.get_args()
 
     def set_http_method( self, method ):
-        pass
+        self.http_method = method
 
     def set_uri( self, uri ):
-        pass
+        self.uri = uri
 
     def add_arg( self, arg ):
-        pass
+        if self.args is None:
+            self.args = dict()
+        self.args[ arg ] = ''
 
     def set_args( self, args ):
-        pass
+        if self.args is None:
+            self.args = dict()
+
+        self.args.update( args )
