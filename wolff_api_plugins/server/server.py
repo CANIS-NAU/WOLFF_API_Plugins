d9a536b341cec73098cdd662c1f128c8f7c4057f
Bob Costas <finkzane@gmail.com>
2019-09-10 18:40:31 -0700

    added simple helper functions

--- wolff_api_plugins/server/server.py
+++ wolff_api_plugins/server/server.py
@@ -5,5 +5,18 @@ class WOLFFServer:
         A server for handling messages sent over 
         the WOLFF network.
     """
-    def __init__( self ):
+    def __init__( self, ip = "127.0.0.1", port = 5555 ):
+        self.ip = ip
+        self.port = port
+
+    def get_ip( self ):
         pass
+
+    def set_ip( self, ip ):
+        self.ip = ip
+
+    def set_port( self, port ):
+        self.port = port
+
+    def get_port( self ):
+        return self.port
