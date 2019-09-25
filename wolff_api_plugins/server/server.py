61b1b13daac018718ac2545288699607016a7294
Bob Costas <finkzane@gmail.com>
2019-09-25 00:45:58 -0700

    added do_request

--- wolff_api_plugins/server/server.py
+++ wolff_api_plugins/server/server.py
@@ -29,6 +29,7 @@ class WOLFFServer:
 
     def do_request( self, data_dict ):
         request_handler = self.get_request_handler( data_dict[ 'credentials' ] )
+        print( "Doing request" )
 
         if data_dict[ 'method' ] == 'get':
             result = getattr( request_handler, data_dict[ 'method' ][ 'http_method' ] )( data_dict[ 'url' ] )
@@ -80,38 +81,55 @@ class MQTTServer( WOLFFServer ):
         super().__init__( ip, port )
         self._channels = channels if channels else None
 
+        self._client = mqtt.Client()
+        self._client.subscribe( 'posts/#' )
+
         def on_connect( client, userdata, flags, rc, channels = None ):
             print( "Connected with result code " + str( rc )  )
 
+            client.subscribe( 'posts/#' )
             for chan in channels:
                 client.subscribe( chan )
 
         def on_message( client, userdata, msg ):
+
             print( msg.topic + " " + str( msg.payload ) )
 
             # get the method name from the URL 
-            data_dict = decode_data( data )
 
-            self.do_request( data_dict )
+            print( "Topic:", msg.topic )
 
-        self.on_connect = lambda client, userdata, flags, rc:
+            data_dict = self.decode_data( msg.payload )
+            topic = str( msg.topic ).split( '/' )
+            topic[ 0 ] = 'responses'
+            # topic.append( data_dict[ 'client_id' ] )
+            topic = '/'.join( topic )
+
+            print( "Doing request." )
+            result = self.do_request( data_dict )
+            print( topic )
+
+            print( "Result: ", result )
+            self.get_client().publish( topic, result.text, qos = 1 )
+
+        self.on_connect = lambda client, userdata, flags, rc: \
                           on_connect( client, userdata, flags, rc, channels = self._channels )
         self.on_message = on_message
 
     def start( self, timeout = 60 ):
-        client = mqtt.Client()
+        self.get_client().on_connect = self.on_connect
+        self.get_client().on_message = self.on_message
 
-        client.on_connect = self.on_connect
-        client.on_message = self.on_message
+        self.get_client().connect( self.get_ip(), self.get_port(), timeout )
 
-        client.connect( self.get_ip(), self.get_port(), timeout )
+        self.get_client().loop_forever()
 
-        client.loop_forever()
-
-        
+    def get_client( self ):
+        return self._client
 
     def add_channel( self, channel ):
         self._channels.append( channel )
+        self._client.subscribe( channel )
 
     def add_channels( self, channels ):
         self._channels += channels
