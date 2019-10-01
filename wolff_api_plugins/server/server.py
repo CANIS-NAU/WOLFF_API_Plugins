23a7ba57b0d66a0194399d08c88d9a24b789b23e
Bob Costas <finkzane@gmail.com>
2019-09-30 22:47:58 -0700

    added method documentation

--- wolff_api_plugins/server/server.py
+++ wolff_api_plugins/server/server.py
@@ -28,16 +28,37 @@ class WOLFFServer:
         return json.loads( data.decode( 'utf-8' ) )
 
     def do_request( self, data_dict ):
-        request_handler = self.get_request_handler( data_dict[ 'credentials' ] )
-        print( "Doing request" )
+        """
+        Perform a request on behalf of the user. Uses the appropriate
+        request handler that allows for OAUth authentication using the users' credentials.
 
-        if data_dict[ 'method' ] == 'get':
+        Args:
+          data_dict: A dictionary containing data that allows 
+                     us to determine the parameters for our request.
+                     
+                     We need the following:
+                        credentials: oauth1 (oauth2 not supported currently) credentials 
+                                     containing the necessary args to perform authenticated requests.
+                        http_method: the http method to perform 
+                        url: the base url + the uri to submit the request to.
+                        params: The paramaters (and their arguments) to include in the request
+        Returns:
+            Result: the result object returned by OAuthRequestsLib
+                             
+        """
+        request_handler = self.get_request_handler( data_dict[ 'credentials' ] )
+
+        if data_dict[ 'method' ][ 'http_method' ] == 'get':
             result = getattr( request_handler, data_dict[ 'method' ][ 'http_method' ] )( data_dict[ 'url' ] )
         else:
             result = getattr( request_handler, data_dict[ 'method' ][ 'http_method' ] )( data_dict[ 'url' ], data = data_dict[ 'method' ][ 'params' ] )
         return result
 
     def start( self ):
+        """
+        Start the server, allow it to run continuously. Requests will be performed 
+        on behalf of users, and responses will be forwarded.
+        """
         with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as sock:
             # bind to the socket 
             sock.bind( ( self.get_ip(), self.get_port() ) )
@@ -49,10 +70,7 @@ class WOLFFServer:
             while True:
                 # accept a connection
                 conn, addr = sock.accept()
-
-                # while true
                 with conn:
-                    
                     # receive message
                     while True:
 
@@ -68,15 +86,29 @@ class WOLFFServer:
                         result = self.do_request( data_dict )
                         conn.sendall( result.content )
 
-
-
-
-
     def get_request_handler( self, credentials ):
+        """
+        Get the request handler that allows us to perform
+        authenticated http requests on behalf of the client.
+        
+        Args:
+           credentials: The user credentials to use for authenticating the user 
+        """
         return OAuth1Session( **credentials )
 
 
 class MQTTServer( WOLFFServer ):
+    """
+    An MQTTServer that performs requests on behalf of the user, similar to 
+    the base WOLFFServer. However, this server interacts with the client through 
+    an MQTT broker.
+    
+    Incoming messages are expected to have the form posts/client_x, where x uniquely identifies 
+    a client. 
+
+    The server expects any requests coming from a client to be in the 'posts/#' topic.
+    The response will be sent to the client via the 'respones/client_x' topic.
+    """
     def __init__( self, ip, port, channels = None ):
         super().__init__( ip, port )
         self._channels = channels if channels else None
@@ -85,6 +117,10 @@ class MQTTServer( WOLFFServer ):
         self._client.subscribe( 'posts/#' )
 
         def on_connect( client, userdata, flags, rc, channels = None ):
+            """
+            Callback used by Paho MQTT upon connection 
+            with the MQTT broker.
+            """
             print( "Connected with result code " + str( rc )  )
 
             client.subscribe( 'posts/#' )
@@ -92,24 +128,22 @@ class MQTTServer( WOLFFServer ):
                 client.subscribe( chan )
 
         def on_message( client, userdata, msg ):
-
-            print( msg.topic + " " + str( msg.payload ) )
+            """
+            Callback used by Paho MQTT upon reception of a message 
+            from the MQTT broker.
+            """
 
             # get the method name from the URL 
-
-            print( "Topic:", msg.topic )
-
             data_dict = self.decode_data( msg.payload )
             topic = str( msg.topic ).split( '/' )
             topic[ 0 ] = 'responses'
             # topic.append( data_dict[ 'client_id' ] )
+
+            # Note: topic is of the form /posts/client_x, where x is the ID for the client
             topic = '/'.join( topic )
 
-            print( "Doing request." )
             result = self.do_request( data_dict )
-            print( topic )
 
-            print( "Result: ", result )
             self.get_client().publish( topic, result.text, qos = 1 )
 
         self.on_connect = lambda client, userdata, flags, rc: \
@@ -117,6 +151,10 @@ class MQTTServer( WOLFFServer ):
         self.on_message = on_message
 
     def start( self, timeout = 60 ):
+        """
+        Start the server, running continuously.
+        Server will handle requests on behalf of the user via the MQTT broker.
+        """
         self.get_client().on_connect = self.on_connect
         self.get_client().on_message = self.on_message
 
