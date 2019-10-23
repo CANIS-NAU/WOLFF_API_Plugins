4ab34cc990723bc8b8c645dbf4d27e8404f603c3
Bob Costas <finkzane@gmail.com>
2019-10-22 20:48:07 -0700

    creation of node_proxy driver

--- node_proxy.py
+++ node_proxy.py
@@ -7,6 +7,31 @@ def main():
                                     "This proxy sits on a node and communicates between a WOLFF client "
                                     "and the WOLFF gateway server."
                                   )
+    argp.add_argument( '--client_ip', help = "The IP address to listen for "
+                       "client connections on. connect to.", type = str,
+                       default = '127.0.0.1' 
+                     )
+    argp.add_argument( '--broker_ip', help = "The IP address to connect to the "
+                       "MQTT broker on.", type = str,
+                       default = '127.0.0.1' 
+                     )
+
+    argp.add_argument( '--client_port', help = "The port to listen for "
+                       "client connections.", type = int, default = 5556
+                     )
+    argp.add_argument( '--broker_port', help = "The port of the MQTT broker",
+                       type = int, default = 1883
+                     )
+
+    args = argp.parse_args()
+
+    server = wolff_server.WOLFFNodeProxy( client_ip = args.client_ip,
+                                          client_port = args.client_port,
+                                          broker_ip = args.broker_ip,
+                                          broker_port = args.broker_port 
+                                        )
+
+    server.start()
 
 if __name__ == '__main__':
     main()
