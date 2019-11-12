4b11cf120bdd56cb52994567b4349b52c0019f9d
Bob Costas <finkzane@gmail.com>
2019-11-11 20:49:02 -0800

    added constructor for Resource

--- wolff_api_plugins/server/resource.py
+++ wolff_api_plugins/server/resource.py
@@ -1,9 +1,27 @@
+import sys
+import os
 
-class ResourceManager:
+class FileResource:
     """
-    Manages a resource, which is something that is 
-    unique to a user/service. 
+    A FileResource is a resource that is located in a file. 
+    This class provides methods for creation, editing, etc. of 
+    FileResources.
+    """
+    def __init__( self, resource_id, base_path, resource_name ):
+        """
+        Create a new FileResource, with resource_id and 
+        a base path.
+        
+        Params:
+          resource_id: The id of this resource. It should be unique.
+          resource_name: The name of the value of the resource. This will 
+                       be the name of the text file.
+        
+          base_path: Base of the path to write files to. 
+                     Files will be written to resource_id/base_path
+        """
+        self._id   = resource_id
+        self._path = base_path
+        self._resource_name = resource_name
 
-    Resources can manage other resources.
-    """
-    pass
+
