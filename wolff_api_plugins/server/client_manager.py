2fbffadc84945d13cd6dd9a4decb51c7dc0da53e
Bob Costas <finkzane@gmail.com>
2019-11-11 21:55:59 -0800

    added ClientManager methods

--- wolff_api_plugins/server/client_manager.py
+++ wolff_api_plugins/server/client_manager.py
@@ -1,4 +1,33 @@
+import resource
+import client
 
 class ClientManager:
-    def __init__( self ):
-        pass
+    def __init__( self, client_dir ):
+        self._dir = client_dir
+        self._clients = _clients_from_dir( client_dir )
+
+    def _clients_from_dir( self, search_dir ):
+        clients = dict()
+
+        # get the full path for each client
+        for root, dirs, files in os.walk( search_dir ):
+            if files:
+                separated_path = root.split( '/' )
+                client_id, service, resource = separated_path
+
+                if client_id not in clients:
+                    clients[ client_id ] = list()
+                clients[ client_id ].append( ( service, resource ) )
+
+        for client, resources in clients.items():
+            new_client = Client( client, resources )
+            self._clients.append( new_client )
+
+    def client_from_dir( path ):
+        client_details = path.split( '/' )
+
+
+
+
+
+
