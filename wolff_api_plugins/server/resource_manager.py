330dd019426c449623d93bc337d675715dbbc0a7
Bob Costas <finkzane@gmail.com>
2019-11-11 16:49:37 -0800

    Creation of resource manager

---
+++ wolff_api_plugins/server/resource_manager.py
@@ -0,0 +1,9 @@
+
+class ResourceManager:
+    """
+    Manages a resource, which is something that is 
+    unique to a user/service. 
+
+    Resources can manage other resources.
+    """
+    pass
