5d02aff9976ca5aadc5797ad183b08bfa547ab5d
Bob Costas <finkzane@gmail.com>
2019-11-12 22:10:41 -0800

    added new ways to create clients

--- wolff_api_plugins/server/client_manager.py
+++ wolff_api_plugins/server/client_manager.py
@@ -13,15 +13,15 @@ class ClientManager:
         for root, dirs, files in os.walk( search_dir ):
             if files:
                 separated_path = root.split( '/' )
-                client_id, service, resource = separated_path
+                base, client_id, service, resource = separated_path
 
                 if client_id not in clients:
                     clients[ client_id ] = list()
                 clients[ client_id ].append( ( service, resource ) )
 
         for client, resources in clients.items():
-            new_client = Client( client, resources )
-            self._clients.append( new_client )
+            new_client = Client( client, resources, base_path = self._dir )
+            self._clients[ new_client.get_id() ] = new_client
 
     def client_from_dir( path ):
         client_details = path.split( '/' )
