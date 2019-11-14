f097fbffc03e6ecc416b6438e60a00e0d31e6a35
Bob Costas <finkzane@gmail.com>
2019-11-13 21:21:41 -0700

    added application enum

--- wolff_api_plugins/server/decoder.py
+++ wolff_api_plugins/server/decoder.py
@@ -1,7 +1,17 @@
+from enum import Enum
+
+class Applications( Enum ):
+    ETSY = 1
 
 class DecoderFactory:
-    def get_decoder( message ):
-        pass
+    def get_decoder( self, message ):
+        application = self.get_service( message )
+
+    def get_service( self, message ):
+        application = message[ 0 ]
+
+        if application == Aplications.ETSY:
+            return EtsyDecoder()
 
 class EtsyDecoder:
     def __init__( self ):
