491ce8f27c787e17b0ea9904a8ff2ca446bd6a9d
Bob Costas <finkzane@gmail.com>
2019-11-17 19:02:51 -0700

    added method docs for Client

--- wolff_api_plugins/server/client.py
+++ wolff_api_plugins/server/client.py
@@ -3,12 +3,35 @@
 from . resource import *
 
 class Client:
+    """
+    A client has a certain number of resources that 
+    are each stored in a file at a path. 
+    """
     def __init__( self, cli_id, resources, base_path = '.' ):
+        """
+        Initialize a client with an id, certain resources, and 
+        a base path from which the client references it sresources.
+        
+        Params:
+           cli_id: Id of the client to create. This ID should be unique to a 
+                  certain directory in which clients are located.
+           resources: The resources a client is created with.
+           base_path: The base path for the client's resources.
+
+        """
         self._id = cli_id
         self._resouces_raw = resources.copy()
         self._resources = self.instantiate_resources( resources, base_path )
 
     def instantiate_resources( self, resources, base ):
+        """
+        Instantiate resources for a client. 
+        
+        Params:
+            resources: A list of resources to instantiate
+            
+            base: the base path for the resources
+        """
         output = dict()
 
         get_val = lambda x: x[ 2 ] if len( x ) == 3 else None
