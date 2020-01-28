441059c344b1f5f112de48a0fd283f52c6c79443
Bob Costas <finkzane@gmail.com>
2020-01-28 10:48:22 -0700

    added argument to include a SQLite3 database

--- server_main.py
+++ server_main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import wolff_api_plugins.server.server as wolff_server
+import wolff_api_plugins.server.DBConnection as wolff_db
 import argparse
 
 def main():
@@ -9,11 +10,16 @@ def main():
                        default = '127.0.0.1' 
                      )
     argp.add_argument( '--port', help = "The port to connect to.", type = int, default = 5555 )
+    argp.add_argument( '--db_file', help = "The name of the file containing a "
+                       "SQLite3 database containing WOLFF information. ",
+                       type = str, default = 'wolff_db.db'
+                     )
 
     args = argp.parse_args()
 
 
-    server = wolff_server.WOLFFServer( ip = args.ip, port = args.port )
+    connection = wolff_db.SQLite3DBConnection( args.db_file )
+    server = wolff_server.WOLFFServer( connection, ip = args.ip, port = args.port )
 
     server.start()
 
