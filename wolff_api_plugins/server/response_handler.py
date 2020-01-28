741b2aeee69b4466328f589bb2be9b0d4b15b43c
Bob Costas <finkzane@gmail.com>
2020-01-28 09:59:08 -0700

    creation of class to handle API responses

---
+++ wolff_api_plugins/server/response_handler.py
@@ -0,0 +1,19 @@
+class ResponseHandler:
+    def __init__( self, db_conn  ):
+        self._conn = db_conn
+
+    def get_handler( self, data_dict ):
+        api_tuple = data_dict[ 'api_details' ]
+        if api_tuple[ 0 ] == 'etsy':
+            if api_tuple[ 1 ] == 'create_listing':
+                return CreateListingResponseHandler( self._conn )
+
+
+
+
+class CreateListingResponseHandler:
+    def __init__( self, db_connection ):
+        self._db = db_connection
+        
+    def handle_response( self, resp_message ):
+        listing_id = json.loads( resp_message )[ 'results' ][ 0 ][ 'listing_id' ]
