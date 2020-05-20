88cc6668c47601223ab5fbccdcdae1a188306bf8
Bob Costas <finkzane@gmail.com>
2020-05-20 10:37:38 -0700

    added logging setup, calculation of time elapsed

--- simple_etsy_client.py
+++ simple_etsy_client.py
@@ -3,7 +3,9 @@ import wolff_api_plugins.client.api_hook as hook
 import wolff_api_plugins.client.client as client
 import wolff_api_plugins.client.server_connection as conn
 import wolff_api_plugins.client.message as message
+import sys
 import time
+import logging
 import argparse
 
 def main():
@@ -17,8 +19,33 @@ def main():
                        help = "The port to bind to.",
                        type = int
                      )
+    argp.add_argument( '--log_file', help = "The name of the file to write log "
+                       "information to.", default = "simple_etsy_client.log"
+                     )
+
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+
 
     cli_args = argp.parse_args()
+
+    logging.basicConfig( level = logging.NOTSET,
+                         format = '%(asctime)s [%(levelname)-5.5s] %(message)s', 
+                         handlers = [
+                             logging.FileHandler( cli_args.log_file ),
+                             logging.StreamHandler( sys.stdout )
+                         ]
+    )
+
+    logging.getLogger().debug( "Parsed arguments with values: \n"
+                               f"--ip: {cli_args.ip}\n"
+                               f"--port: {cli_args.port}\n"
+                               f"--log_file: {cli_args.log_file}\n"
+    )
+
+
 
     args = { 'quantity': 0, 'title': '', 'description': '',
                 'price': 0, 'who_made': '', 'is_supply': True, 'when_made': '',
@@ -48,6 +75,8 @@ def main():
                             )
 
 
+    start_time = time.time()
+
     etsy_client = client.Client( connection = connect, endpoint = etsy_hook,
                                  message_type = message.EtsyMessage
                                 )
@@ -56,14 +85,13 @@ def main():
     resp = etsy_client.create_listing( quantity = 2, title = "title_1",
             description = "desc_1",
             price = 1.11, who_made = 'i_did', is_supply = True,
-            when_made = 'made_to_order', shipping_template_id = 76575991147
+            when_made = 'made_to_order', shipping_template_id = 84634415230
             )
 
-
+    end_time = time.time()
     # response can take a little bit
-    # etsy_client.get_connection().subscribe_to( 'responses/client_1' )
-    # result = etsy_client.get_connection().check_for_messages()
-    print( resp )
+    logging.getLogger().info( f"Elapsed time: {end_time - start_time}" )
+    logging.getLogger().debug( f"ID received from server: {int.from_bytes( resp, 'big' )}" )
 
 
 if __name__ == '__main__':
