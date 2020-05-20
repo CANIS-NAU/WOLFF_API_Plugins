d07a3bb75b0cdd5effb191724484ec1d90e64542
Bob Costas <finkzane@gmail.com>
2020-05-20 12:22:34 -0700

    added the complete output of CLI args, get_listing_data method

--- update_listing.py
+++ update_listing.py
@@ -14,7 +14,7 @@ def main():
     argp.add_argument( '--listings_file', help = "Name of the file containing listings that can be updated.",
                        default = "submitted_listings.json"
     )
-    argp.add_argument( '--update', action = 'append', nargs = '+',
+    argp.add_argument( '--update', action = 'append',
                        help = "The value you wish to update, combined with its new value, separated by an 'equals'. "
                        'Example: title="This is the new title". To update multiple values, include this argument multiple times.'
     )
@@ -22,8 +22,12 @@ def main():
                        "information to.", default = "update_listing.log"
                      )
 
-    
     args = argp.parse_args()
+
+    update_str = ''
+    for update in args.update:
+        update_str += f'--update {update}\n'
+        
 
     logging.basicConfig( level = logging.NOTSET,
                          format = '%(asctime)s [%(levelname)-5.5s] %(message)s', 
@@ -36,9 +40,29 @@ def main():
     logging.getLogger().debug( "Parsed arguments with values: \n"
                                f"--ip: {args.ip}\n"
                                f"--port: {args.port}\n"
+                               f"--listing_id: {args.listing_id}\n"
+                               f"--listings_file: {args.listings_file}\n"
+                               f"{update_str}"
                                f"--log_file: {args.log_file}\n"
     )
 
+    listings = list()
+
+    logging.getLogger().debug( f"Attempting to open {args.listings_file} to read listings" )
+    with open( args.listings_file, 'r' ) as of:
+        listings = get_listing_data( of )
+
+    logging.getLogger().debug( "Successfully parsed listing data" )
+
+
+def get_listing_data( file_handle ):
+    output = list()
+
+    for line in file_handle:
+        new_listing = json.loads( line.strip() )
+        logging.getLogger().debug( f"Data from file: {new_listing}" )
+        output.append( new_listing )
+    return output
 
 
 
