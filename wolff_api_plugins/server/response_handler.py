85b6d01ce14dbcd4f9a32c8da4e77b61c9c8e706
Bob Costas <finkzane@gmail.com>
2020-05-26 11:39:45 -0700

    added response_handler for checklistingstock

--- wolff_api_plugins/server/response_handler.py
+++ wolff_api_plugins/server/response_handler.py
@@ -14,6 +14,16 @@ class ResponseHandler:
                 )
                 return CreateListingResponseHandler( self._conn )
 
+            elif api_tuple[ 1 ] == 'check_listing_stock':
+                logging.getLogger().debug( "Creating a ResponseHandler for "
+                                           "etsy/check_listing_stock"
+                )
+                return CheckListingStockResponseHandler( self._conn )
+
+            else:
+                logging.getLogger().error( "{api_tuple[ 0 ]}/{api_tuple[ 1 ]} is not a valid service/method specifier." )
+                raise ValueError( "Invalid service/method specifier included" ) 
+
 
 
 
@@ -26,3 +36,15 @@ class CreateListingResponseHandler:
         listing_id = json.loads( resp_message )[ 'results' ][ 0 ][ 'listing_id' ]
         logging.getLogger().debug( f"ID Retrieved: {listing_id}" )
         return self._db.add_listing( listing_id, client_id )
+
+
+class CheckListingStockResponseHandler:
+    def __init__( self, db_connection ):
+        self._db = db_connection
+
+    def handle_response( self, resp_message, client_id ):
+        logging.getLogger().warning( "CheckListingStockResponseHandler.handle_response is currently not implemented. "
+                                     "It uses a dummy value that can be used to simulate a response from the server."
+                                   )
+
+        return bytearray( [ 0x01, 0x02, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ] )
